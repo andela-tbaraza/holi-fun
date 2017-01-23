1a3e0e51b62fada508a10f2bbab8cb0d5bcfda61
Tonida Baraza <tonida.baraza@andela.com>
2017-01-23 11:55:26 +0300

    add AboutPage component

---
+++ app/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>About us</h1>
+        <p>I dont know what to say yet</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
