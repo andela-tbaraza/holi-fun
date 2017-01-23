af5239fb89db40547ca427770cd0b5cb2f31846e
Tonida Baraza <tonida.baraza@andela.com>
2017-01-23 11:51:14 +0300

    add homepage tests

--- tests/homePage.test.js
+++ tests/homePage.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import expect from 'expect';
 import { mount, shallow } from 'enzyme';
-import Home from '../app/components/HomePage';
+import Home from '../app/components/home/HomePage';
 
 describe('Home page', () => {
   it('should show content', () => {
-    const wrapper = shallow(<Home/>);
+    const wrapper = shallow(<Home />);
     expect(wrapper.find('h1').text()).toEqual('Holi-fun');
   });
 });
