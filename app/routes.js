4ece45565579e0929d8e70336751731e1d810ecf
Tonida Baraza <tonida.baraza@andela.com>
2017-01-23 11:54:06 +0300

    setup initial routes

---
+++ app/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import App from './components/App';
+import HomePage from './components/home/HomePage';
+import AboutPage from './components/about/AboutPage';
+import InterestsPage from './components/InterestsPage';
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={HomePage} />
+    <Route path="about" component={AboutPage} />
+    <Route path="interests" component={InterestsPage} />
+  </Route>
+);
