5d1b5324584d4fb92c96cc4750a32595b9f524b6
Tonida Baraza <tonida.baraza@andela.com>
2017-01-23 08:41:55 +0300

    add provider component

--- app/index.js
+++ app/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/HomePage.jsx';
+import { Router, browserHistory } from 'react-router';
+// import 'babel-polyfill';
+import { Provider } from 'react-redux';
+import routes from './routes';
+import './styles/styles.css';
+import configureStore from './store/configureStore';
+// import './node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<Home />, document.getElementById('app'));
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
+  document.getElementById('app')
+);
